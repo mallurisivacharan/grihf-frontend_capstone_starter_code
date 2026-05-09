06987d98b5d3c059656a5e7051ce8038cb02d8ff
mallurisivacharan <mallurisivacharan@gmail.com>
2026-05-09 14:27:37 +0530

    login api added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setLoginData({
+      ...loginData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const loginUser = async () => {
+    try {
+      const response = await fetch("https://example-api.com/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginData),
+      });
+
+      const data = await response.json();
+
+      console.log(data);
+
+      alert("Login Successful");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <h2>Login Form</h2>
+      <h2>Login</h2>
 
-      <input type="email" placeholder="Email" />
-      <br /><br />
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        onChange={handleChange}
+      />
 
-      <input type="password" placeholder="Password" />
-      <br /><br />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        onChange={handleChange}
+      />
 
-      <button>Login</button>
+      <button onClick={loginUser}>Login</button>
     </div>
   );
 }
